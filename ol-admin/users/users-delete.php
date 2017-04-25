de9316bc54f3476bb28461ed8e1f38daeada74ab
Poorya Shadfar <Poorya_gds@yahoo.com>
2017-04-25 18:02:45 +0430

    Update users-delete.php

--- ol-admin/users/users-delete.php
+++ ol-admin/users/users-delete.php
@@ -3,6 +3,6 @@ require_once("../../config.php");
  $sql = "DELETE FROM `ol-user` WHERE id = '".$_POST["id"]."'";  
  if(mysqli_query($connect, $sql))  
  {  
-      echo 'اين فيلد با موفقيت حذف شد';  
+      echo 'The data were removed successfully';  
  }  
  ?> 
