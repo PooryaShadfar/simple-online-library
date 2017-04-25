bda4a1cd72ccb4164d90c2bba8c27b19643c403c
Poorya Shadfar <Poorya_gds@yahoo.com>
2017-04-25 17:48:42 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once("config.php"); ?>
 <?php include_once("header.php"); ?>
 <section id="firstCase" class="sepratorContains"><div class="container"><div class="row">
-<div class="titlebar"><h4><i class="fa fa-book"></i> آخرین کتاب ها  </h4></div>
+<div class="titlebar"><h4><i class="fa fa-book"></i> Last Books  </h4></div>
 <div class="col-md-12 centerLayout">
 <?php include('show-posts.php' ); ?>
 </div>
