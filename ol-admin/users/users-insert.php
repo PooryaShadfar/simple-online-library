6ac0516e93907406a0a6dfc6b9f7bb56e854937f
Poorya Shadfar <Poorya_gds@yahoo.com>
2017-04-25 18:01:55 +0430

    Update users-insert.php

--- ol-admin/users/users-insert.php
+++ ol-admin/users/users-insert.php
@@ -3,6 +3,6 @@ require_once("../../config.php");
 $sql = "INSERT INTO `ol-user` (`id`, `username`, `password`, `role`) VALUES('','".$_POST["username"]."', '".$_POST["password"]."', '".$_POST["role"]."')";  
 if(mysqli_query($connect, $sql))  
 {   
-   echo 'داده ها وارد شدند.';  
+   echo 'The data arrived successfully';  
 }  
 ?> 
