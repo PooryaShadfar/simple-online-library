743648c0d94ba0797b42b12a68ca726633e139c5
Poorya Shadfar <Poorya_gds@yahoo.com>
2017-04-25 17:46:35 +0430

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php include_once("header.php"); ?>
 <section id="firstCase" class="sepratorContains"><div class="container"><div class="row">
-<div class="titlebar"><h4><i class="fa fa-warning" aria-hidden="true"></i> 404 پیدا نشد  </h4></div>
-<h3>برگه مورد نظر شما یافت نشد</h3>
-<h4>لطفا به صفحه اصلی بازگردید و یا وارد حساب کاربری خود شوید.</h4>
+<div class="titlebar"><h4><i class="fa fa-warning" aria-hidden="true"></i> 404 not found! </h4></div>
+<h3>Oops Something is Wrong. </h3>
+<h4>We Cant Find Any Book For You Try Again Please!</h4>
 </div></div></section><div style="margin-bottom:30em;"></div>
 <?php include_once("footer.php"); ?>
