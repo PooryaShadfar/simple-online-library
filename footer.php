9205833825929fb07f903e3bfced6dbada88bc1c
Poorya Shadfar <Poorya_gds@yahoo.com>
2017-04-25 17:47:04 +0430

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 		</div>
 	</div>
 		<div id="footer-copy"><div class="container"><div class="row">
-	<span class="footer-bottom-sp pull-left"><p> طراحی شده با <i style="color:red;"class="fa fa-heart faa-pulse animated"></i> توسط <a href="http://pgraph.ir/" target="_blank">
+	<span class="footer-bottom-sp pull-left"><p> Made With <i style="color:red;"class="fa fa-heart faa-pulse animated"></i> By <a href="http://pgraph.ir/" target="_blank">
 	<img style="width: 65px;"src="ol-apperance/image/pg-logo.png" alt="Pgraph" /></a></p></span></div></div>
 		</div>
 </footer>
